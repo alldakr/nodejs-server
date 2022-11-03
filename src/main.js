eaee6de535fef00b1483b0b083e58177f52f0319
alldakr <alldakr@gmail.com>
2022-11-04 01:54:19 +0900

    ok: Part1 - Ch5 - 프레임워크 없이 간단한 RESTful API 서버 만들어보기

--- src/main.js
+++ src/main.js
@@ -8,3 +8,65 @@
  * - 인증 로직은 넣지 않습니다.
  * - RESTful API를 사용합니다.
  */
+
+const http = require('http')
+const { routes } = require('./api')
+
+const server = http.createServer((req, res) => {
+  async function main() {
+    const route = routes.find(
+      (_route) =>
+        req.url &&
+        req.method &&
+        _route.url.test(req.url) &&
+        _route.method === req.method
+    )
+
+    if (!req.url || !route) {
+      res.statusCode = 404
+      res.end('Not Found')
+      return
+    }
+
+    const regexResult = route.url.exec(req.url)
+
+    if (!regexResult) {
+      res.statusCode = 404
+      res.end('Not Found')
+      return
+    }
+
+    /** @type {Object.<string, *> | undefined} */
+    const reqBody =
+      (req.headers['content-type'] === 'application/json' &&
+        (await new Promise((resolve, reject) => {
+          req.setEncoding('utf-8')
+          req.on('data', (data) => {
+            try {
+              resolve(JSON.parse(data))
+            } catch {
+              reject(new Error('Ill-formed json'))
+            }
+          })
+        }))) ||
+      undefined
+
+    const result = await route.callback(regexResult, reqBody)
+    res.statusCode = result.statusCode
+
+    if (typeof result.body === 'string') {
+      res.end(result.body)
+    } else {
+      res.setHeader('Content-Type', 'application/json; charset=utf-8;')
+      res.end(JSON.stringify(result.body))
+    }
+  }
+
+  main()
+})
+
+const PORT = 4000
+
+server.listen(PORT, () => {
+  console.log(`The server is listening at port: ${PORT}`)
+})
